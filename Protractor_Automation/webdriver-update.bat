105e1060619666bfb1f8cbeb92afe3d76b1ea5f7
TithiShah <39796115+TithiShah@users.noreply.github.com>
2018-06-05 15:26:56 +0530

    Update webdriver-update.bat

--- Protractor_Automation/webdriver-update.bat
+++ Protractor_Automation/webdriver-update.bat
@@ -1,1 +1,1 @@
-webdriver-manager update --ignore_ssl --ie & webdriver-manager update --ignore_ssl
+webdriver-manager update --ignore_ssl --gecko=false --chromedriver=true --ie=true & webdriver-manager start --ignore_ssl --gecko=false
